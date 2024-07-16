25b1668229443a82bfdcfad47c508244207c16e8
M-kalebu <kalebulot@gmail.com>
2024-07-16 10:04:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,428 @@
+// document.write("Hello World!");
+// window.alert("click me to close");
+// console.log("iam form console")
+// try{
+//     {
+//         let x = 10;
+//     }
+//     console.log(x);
+// }
+
+// catch(err){
+//     console.log(err);
+// }
+// finally{
+//     console.log("naaku burra ledhu");
+// }
+
+// import app2 from "./app2.js"
+// var x = {
+//     name : "kumar",
+//     age : 18,
+// }
+// x.number = app2
+
+// export default x;
+
+// var a = parseInt(window.prompt("Enter A value"));
+// var b = parseInt(window.prompt("Enter B value"));
+// var c = parseInt(window.prompt("Enter C value"));
+// var s = a + b + c;
+// console.log(Math.sqrt(s*(s-a)*(s-b)*(s-c)));
+
+// 'u'
+
+// let name;
+// console.log(name);
+
+// let fullName = "MudadlaKalebu";
+// console.log(fullName);
+
+//        !!!------Variables---------!!!!
+
+// let name = "MudadlaKalebu";
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+
+// let address, pin, state;
+// address = "ctri";
+// pin = "533101";
+// state = "andhrapradesh";
+// name = "krathi Kumar"
+// console.log(address,pin,state,name);
+
+// let random_1234_$ = "SaiKumar";
+// console.log(random_1234_$);
+
+// // using let
+// let firstName = "kalebu";
+
+// // using var
+// var lastName = "Mudadla Kalebu";
+// // var lastName = "mass";
+
+// // using const
+// const middleName = "NoName";
+// // const middleName = "Name" !
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(middleName);
+
+// const firstName = "Kalebu";
+// const lastName = "Mudadla";
+// // let fullName = firstName+lastName; Do it this method also
+// let fullName;
+// console.log("My fullName is:" + firstName+ " " + lastName);
+
+// let website = "Google";
+// let url = "https://www." + website + ".com";
+// console.log(url);
+
+//    !!!!Numbers------------->
+// const firstnumber = 120;
+// let secondnumber = 120;
+// var add = firstnumber + secondnumber;
+// console.log(secondnumber);
+// console.log(firstnumber);
+// console.log(add);
+
+// var number = 123;
+// // number = number + 10
+// number += 10;
+// number -= 10;
+// number *= 10;
+// number /= 10;
+// console.log(number);
+
+// let number = 123;
+// number++;
+// number++;
+// number--
+// console.log(number);
+
+// var number = 100;
+// var reminder = number %2;
+
+// console.log(reminder);
+
+// let number = 2 + 5 - 10 * (10 + 20);
+// let number2 = (10*2)+12+12;
+// console.log(number);
+// console.log(number2);
+
+// ---> string
+// const k = "Hello";
+// console.log(typeof k);
+
+// ---->Number
+// const k = 21
+// console.log(typeof k);
+
+// ------> Boolean
+// var value1 = true;
+// var value2 = false;
+// console.log(typeof value1);
+// console.log(typeof value2);
+
+// const name = null;
+// console.log(typeof name);
+
+// let name;
+// console.log(name)
+
+// {
+//     var n = 1235
+// }
+// console.log(n);
+
+// var fruitsList = ["apples","bannana","pineapple","orange","watermelon",
+//     123,true,253.25,false
+// ];
+// console.log(fruitsList);
+// // console.log(fruitsList[2]);
+
+// fruitsList[5] = "carrot"
+// console.log(fruitsList);
+
+// Functions------->
+
+// function invoke(){
+//     console.log("myself kalebu");
+//     console.log("pursuing B.Tech");
+//     console.log("3rd Year");
+// }
+// invoke();
+// // some 100 lines of code
+// invoke();
+// // some lines of code
+// invoke();
+//     // after some lines of code
+// invoke();
+
+// function invoke(name){
+//     console.log("Hello" + name);
+// }
+// invoke("name");
+
+// function invoke(name){
+//     console.log("Full" + name);
+// }
+// invoke("Name");
+
+// function invoke(name,study){
+//     console.log("Hello" + name + study);
+// }
+
+// invoke("kalebu",182003)
+
+// function calculate(value){
+//     // return newValue = value * 2.54;
+//     // console.log("The number in cm"+ value * 2.54 + "cm");
+//     // return value * 2.54;
+// }
+
+// const width = calculate(100);
+// const height = calculate(80);
+
+// const dimensions = [width,height]
+// console.log(dimensions)
+
+// function addNumbers(num1,num2){
+//     return num1 + num2;
+// }
+
+// const firstvalue = addNumbers(2,5);
+// const secondvalue = addNumbers(70,30);
+
+//         // function expression
+// const newValue = function (num1,num2){
+//     return num1 + num2;
+// }
+// const thirdvalue = newValue(100,50)
+
+// const result = [firstvalue,secondvalue,thirdvalue];
+// console.log(result);
+
+//   ---------> object Function <-------------
+
+// const person = {
+//     FullName : "kalebu",
+//     LastName : "Mudadla",
+//     age : 21,
+//     eduction : true,
+//     married :false,
+//     friends : ["Bhanu","Athreya"],
+//     greetings: function () {
+//         console.log("Hey Kalebu");
+//     },
+// };
+// console.log(person);
+// person.greetings()
+// console.log(person.age);
+
+// console.log(person["FullName"]);
+// console.log(typeof person)
+// console.log(typeof person.friends)
+
+// --------------->Condtions
+
+// const value1 = 2 > 1;
+
+// if(value1){
+//     console.log("Hello EveryOne");
+// }
+// else{
+//     console.log("Heyy!")
+// }
+
+// const value1 = 4
+// const value2 = 7
+
+// if(value1 > value2){
+//     console.log("first number is greaterthan secondnumber")
+// }
+// else{
+//     console.log("second number is greater than to first number")
+// }
+
+// var n = window.prompt("Enter a number");
+
+// if(0 < n){
+//     document.write("positive Number")
+// }
+// else{
+//     document.write("negative number")
+// }
+
+// const num1 = 10;
+// const num2 = 10;
+
+// const value1 = num1 != num2;
+// console.log(value1);
+
+// const value2 = num1 === num2;
+// console.log(value2)
+
+// const UserName = "rom";
+// const age = 18;
+// if(UserName ==="rom" && age === 18){
+//     console.log("correct values")
+// }
+// else{
+//     console.log("Invalid details")
+// }
+
+// function fun(){
+//     // document.getElementById('demo').style.fontSize = '36px'
+//     document.getElementById('demo').style.display = 'none'
+// }
+
+//
+
+// Switch Statement
+
+// const dice = 5;
+
+// switch (dice) {
+//   case 1:
+//     console.log("you got one");
+//     break;
+//   case 2:
+//     console.log("you got two");
+//     break;
+//   case 3:
+//     console.log("you got three");
+//     break;
+//   case 4:
+//     console.log("you got four");
+//     break;
+//   case 5:
+//     console.log("you got five");
+//     break;
+//   case 6:
+//     console.log("you got six");
+//     break;
+//   default:
+//     console.log("You didn't roll the dice");
+// }
+
+    // While loop
+
+// let amount = 10;
+
+// while(amount > 0){
+//     console.log("i have" + " " + amount + "dollars so i will go to shooping mall");
+//     amount--; // amount = amount - 1;
+// }
+
+// do while loop
+// let amount = 7;
+
+// do{
+//     console.log("i have" + " " + amount + "dollars" );
+//     amount++;
+// }
+// while(amount <= 10)
+
+// let i;
+// for(i=0; i<=10 ;i++){
+//     console.log("The number is:" + i);
+// }
+
+// let i;
+// for(i=10; i>=0;i--){
+//     console.log("The number is:" + i);
+// }
+
+// let text = "KaLebu";
+// console.log(text.toLowerCase());
+
+
+
+// const person = {
+//     name : "kalebu_kings",
+//     grettings : function(){
+//         console.log("Heyy How are you !!")
+//         return null;   // we can use return to remove the undefined
+//     },
+// };
+//     console.log(person);
+//     console.log(person.name);
+//     console.log(person.grettings());
+
+
+// let text = "kalebu_kings"  
+//     console.log(text.length);
+//     console.log(text.toLowerCase())
+//     console.log(text.toUpperCase());
+//     console.log(text.charAt(text.length - 1));  
+//     console.log(text.indexOf("u"));      //index value indicates
+//     console.log(text.startsWith("kalebu_kings"));
+//     console.log(text);
+//     console.log(text.trim())  //space trimmer
+//     console.log(text.trim().toLowerCase().startsWith("kalebu_kings"));
+//     console.log(text.toLowerCase().includes("kale"));  //true or false
+//     console.log(text.slice(3,6));
+//     console.log(text.slice(4));
+
+
+// const firstName = "kalebu";
+// const age = 21;
+
+// const sentence = "Hey its" + " " + firstName + " she age is " + age;
+
+// console.log(sentence);
+
+// const value = `hey its ${firstName} she age is ${age}`;
+// console.log(value);
+
+
+// let fruitsNames = ["bannana","apples","oranges","grapes","pineapple"];
+// console.log(fruitsNames);
+// console.log(fruitsNames[2]);
+// concate
+// let firstNames = ["emma","ron","alloy"]
+// let allNames = fruitsNames.concat(firstNames);
+// console.log(allNames)
+//reverse
+// console.log(allNames.reverse());
+//push
+// allNames.push("cherries");
+// console.log(allNames)
+
+//pop
+// allNames.pop()
+// allNames.pop()
+// console.log(allNames)
+
+//unshift----->         adding starting array 
+// allNames.unshift("blackberries");
+// console.log(allNames);
+
+// allNames.shift();
+// console.log(allNames);
+
+
+// Array problem
+
+// let firstNames = ["kalebu","vinay","mahesh"];
+// const lastName = "Mudadla"
+// let newArray =[];
+
+// for(let i = 0;i < firstNames.length;i++){
+//     // console.log(i)
+//     // console.log(firstNames[i])
+
+//     let fullName = `${firstNames[i]}  ${lastName}`;
+//     newArray.push(fullName);
+// }
+
+// console.log(firstNames)
+// console.log(newArray);
+
+
